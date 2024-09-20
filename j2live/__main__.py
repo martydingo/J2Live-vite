fe741e9d95372a1d0d08acbbacf4043cfc250479
Martin George <marty@dingo.foo>
2024-09-20 13:02:00 +0100

    Initial

--- j2live/__main__.py
+++ j2live/__main__.py
@@ -1,12 +1,13 @@
 from .api import API
 
-import uvicorn
-
-# from argparse import ArgumentParser
-# argParser = ArgumentParser()
-
-# argParser.add_argument()
+import uvicorn, asyncio
 
 j2live_api = API()
 
-uvicorn.run(j2live_api.API)
+taskGroup = asyncio.TaskGroup()
+
+async def main():
+    async with asyncio.TaskGroup() as task:
+        task.create_task(uvicorn.run(j2live_api.API))
+
+main()
